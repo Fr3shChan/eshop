1205f3fe0e953d63f77d5c7b25bd968448cf6bac
Marcin Gierszewski <mgierowski61@gmail.com>
2024-12-12 20:08:48 +0100

    poprawki - rozpoczęcie ekranu logowania

--- src/modules/Cart/index.js
+++ src/modules/Cart/index.js
@@ -76,7 +76,7 @@ const Cart = () => {
                     <div className="flex flex-col justify-between ml-4 flex-grow">
                       <span className="font-bold text-sm">{cart?.title}</span>
                       <span className="text-red-500 text-xs capitalize">{cart?.category}</span>
-                      <div className="font-semibold hover:text-red-500 text-gray-500 text-xs cursor-pointer" onClick={() => removeProduct(cart?.id)}>Remove</div>
+                      <div className="font-semibold hover:text-red-500 text-gray-500 text-xs cursor-pointer" onClick={() => removeProduct(cart?.id)}>Usuń</div>
                     </div>
                   </div>
                   <div className="flex justify-center w-1/5">
@@ -99,33 +99,36 @@ const Cart = () => {
           <Link to={'/products'} className="flex font-semibold text-indigo-600 text-sm mt-10">
 
             <svg className="fill-current mr-2 text-indigo-600 w-4" viewBox="0 0 448 512"><path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" /></svg>
-            Continue Shopping
+            Kontynuuj zakupy
           </Link>
         </div>
 
         <div id="summary" className="w-1/4 px-8 py-10">
-          <h1 className="font-semibold text-2xl border-b pb-8">Order Summary</h1>
+          <h1 className="font-semibold text-2xl border-b pb-8">Podsumowanie zamówienia</h1>
           <div className="flex justify-between mt-10 mb-5">
-            <span className="font-semibold text-sm uppercase">Items {carts?.length}</span>
+            <span className="font-semibold text-sm uppercase">Rzeczy {carts?.length}</span>
             <span className="font-semibold text-sm">{total?.toFixed(2)}$</span>
           </div>
           <div>
-            <label className="font-medium inline-block mb-3 text-sm uppercase">Shipping</label>
+            <label className="font-medium inline-block mb-3 text-sm uppercase">Dostawa</label>
             <select className="block p-2 text-gray-600 w-full text-sm">
-              <option>Standard shipping - $10.00</option>
+              <option>Poczta Polska - $10.00</option>
+              <option>DHL Express - $19.00</option>
+              <option>Inpost Paczkomat - $6.99</option>
+              <option>Inpost Kurier - $11.99</option>
             </select>
           </div>
           <div className="py-10">
-            <label for="promo" className="font-semibold inline-block mb-3 text-sm uppercase">Promo Code</label>
+            <label for="promo" className="font-semibold inline-block mb-3 text-sm uppercase">Kod promocyjny</label>
             <input type="text" id="promo" placeholder="Enter your code" className="p-2 text-sm w-full" />
           </div>
-          <button className="bg-red-500 hover:bg-red-600 px-5 py-2 text-sm text-white uppercase">Apply</button>
+          <button className="bg-red-500 hover:bg-red-600 px-5 py-2 text-sm text-white uppercase">Zatwierdź</button>
           <div className="border-t mt-8">
             <div className="flex font-semibold justify-between py-6 text-sm uppercase">
-              <span>Total cost</span>
+              <span>Do zapłaty</span>
               <span>${(total + 10).toFixed(2)}</span>
             </div>
-            <button className="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">Checkout</button>
+            <button className="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">Do kasy</button>
           </div>
         </div>
 
